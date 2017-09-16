fc6b133e59a6d992b4f604943cf372813cbe87fb
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-09-16 11:52:26 +0200

    First try

---
+++ src/util.js
@@ -0,0 +1,55 @@
+/// @ts-check
+
+var checker = null;
+var metadata = {}
+
+// Files have .txt extension to allow gzipping in Github Pages
+var references = [
+    "mscorlib.dll",
+    "System.dll",
+    "System.Core.dll",
+    "System.Data.dll",
+    "System.IO.dll",
+    "System.Xml.dll",
+    "System.Numerics.dll",
+    "FSharp.Core.sigdata",
+    "FSharp.Core.dll",
+    "Fable.Core.dll"
+];
+
+function isSigdata(ref) {
+    return ref.indexOf(".sigdata") >= 0;
+}
+
+function getFileBlob(key, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url + ".txt");
+    xhr.responseType = "arraybuffer";
+    xhr.onload = function (oEvent) {
+      var arrayBuffer = xhr.response;
+      if (arrayBuffer) {
+        metadata[key] = new Uint8Array(arrayBuffer);
+      }
+    };
+    xhr.onerror = function (oEvent) {
+      console.log('Error loading ' + url);
+    };
+    xhr.send();
+};
+
+references.forEach(function(fileName){
+    getFileBlob(fileName, '/repl/metadata/' + fileName);
+});
+
+export function getChecker(createChecker) {
+    if (checker === null) {
+        if (Object.getOwnPropertyNames(metadata).length < references.length) {
+            return null;
+        }
+        var readAllBytes = function (fileName) { return metadata[fileName]; }
+        var references2 = references.filter(x => !isSigdata(x)).map(x => x.replace(".dll", ""));
+        checker = createChecker(references2, readAllBytes)
+    }
+    return checker;
+}
+
