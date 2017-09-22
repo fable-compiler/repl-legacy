1f7758a90d122c690a2a56bd06cbbf820f511190
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-09-22 03:02:20 +0200

    Edit chart from repl

--- src/App/app.fs
+++ src/App/app.fs
@@ -7,9 +7,13 @@ open Fable.Import
 open Fable.Recharts
 open Fable.Recharts.Props
 open Fulma.Layouts
+open Fulma.BulmaClasses
+open Fulma.Elements
 module R = Fable.Helpers.React
 module RProps = Fable.Helpers.React.Props
 
+[<Emit("window[$0] = $1")>]
+let makeGlobal (key: string) (value: obj): unit = jsNative
 let create(elId: string): unit = importMember "editor"
 
 type [<Pojo>] MyData =
@@ -25,6 +29,18 @@ let data =
        { name = "Page G"; uv = 3490.; pv = 4300.; amt = 2100. }
     |]
 
+type [<Pojo>] Props<'T> = { InitData: 'T; Render: 'T->React.ReactElement }
+
+type [<Pojo>] State<'T> = { Data: 'T }
+
+type Component<'T>(props) =
+    inherit React.Component<Props<'T>,State<'T>>(props)
+    do base.setInitState({ Data = props.InitData })
+    member this.componentDidMount() =
+        makeGlobal "setData" (fun v -> this.setState({ Data = v }))
+
+    member this.render() =
+        this.props.Render(this.state.Data)
 
 let barChart(data) =
     let margin = { top=5.; bottom=5.; right=20.; left=0. }
@@ -38,18 +54,35 @@ let barChart(data) =
     ]
 
 let root() =
-    Columns.columns [] [
-        Column.column [] [
-            R.div [RProps.ClassName "editor-frame"] [
-                R.div [
-                    RProps.Id "fable-repl"
-                    RProps.Style [RProps.CSSProp.Height "600px"]
-                ] []
+    R.div [] [
+        Hero.hero [ (*Hero.isSuccess; Hero.isMedium*) ] [
+            Hero.body [ ] [
+                Level.level [] [
+                    Level.item [] [
+                        R.img [
+                            RProps.Src "img/fable_ionide.png"
+                            RProps.Style [RProps.MaxWidth "200px"]
+                        ]
+                    ]
+                    Level.item [] [
+                        Heading.h1 [] [ R.str "Fable Ionide" ]  
+                    ]
+                ]
             ]
         ]
-        Column.column [] [
-            R.div [RProps.Style [RProps.MarginTop "100px"]] [
-                barChart data
+        Columns.columns [] [
+            Column.column [] [
+                R.div [RProps.ClassName "editor-frame"] [
+                    R.div [
+                        RProps.Id "fable-repl"
+                        RProps.Style [RProps.CSSProp.Height "600px"]
+                    ] []
+                ]
+            ]
+            Column.column [] [
+                R.div [RProps.Style [RProps.MarginTop "100px"]] [
+                    R.com<Component<Array>,_,_> { InitData = data; Render = barChart } []
+                ]
             ]
         ]
     ]
